2c7cfbeefee201c0be9a6fbc1d27e2676673a735
Georgii Savin <sawin.georgij@yandex.ru>
2021-09-14 15:54:38 +0300

    Refactor chart drawGraphic - extract function, introduce explaining variable, introduce foreign method,

--- src/components/Home/Graphic.js
+++ src/components/Home/Graphic.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import agent from "../../agent";
 import {POINT_ADDED, POINTS_RECALCULATED} from "../../constants/actionTypes";
+import {drawArrow, drawRectangle, drawSector, drawTriangle, drawXAxisLabels, drawYAxisLabels} from "./chart.utils";
 
 const mapStateToProps = state => ({
     points: state.points.points_r,
@@ -76,151 +77,51 @@ class Graphic extends React.Component {
 
     }
 
-    addPointFromCanvas(ev) {
-        console.log(`Click on ${this}`);
+    addPointFromCanvas(event) {
 
         let br = this.graphic.getBoundingClientRect();
         let left = br.left;
         let top = br.top;
 
-        let event = ev;
-        let x = event.clientX - left;
-        let y = event.clientY - top;
+        const x = event.clientX - left;
+        const y = event.clientY - top;
         console.log(`x=${x} y=${y}`);
 
-        let xCalculated = (x - 150) / 130 * 5;
-        let yCalculated = (-y + 150) / 130 * 5;
+        const xCalculated = (x - 150) / 130 * 5;
+        const yCalculated = (-y + 150) / 130 * 5;
 
         this.props.onCanvasClick(xCalculated, yCalculated, this.props.rc || 1);
 
     }
 
-    drawGraphic(r) {
-        console.log("Drawing graphic with R="+r);
-        let context = this.graphic.getContext("2d");
-        context.clearRect(0, 0, this.graphic.width, this.graphic.height);
+    drawGraphic(radius) {
+        const areaColor = "#2f9aff";
+        const axesColor = "black";
+        const ctx = this.graphic.getContext("2d");
+        ctx.clearRect(0, 0, this.graphic.width, this.graphic.height);
 
-        // rectangle
-        context.beginPath();
-        context.rect(150, 150-r*13, 130*(r/5), (r)*13);
-        context.closePath();
-        context.strokeStyle = "#2f9aff";
-        context.fillStyle = "#2f9aff";
-        context.fill();
-        context.stroke();
-
-        // sector
-        context.beginPath();
-        context.moveTo(150, 150);
-        context.arc(150, 150, 65*(r/5), 0, Math.PI/2, false);
-        context.closePath();
-        context.strokeStyle = "#2f9aff";
-        context.fillStyle = "#2f9aff";
-        context.fill();
-        context.stroke();
-
-        // triangle
-        context.beginPath();
-        context.moveTo(150, 150);
-        context.lineTo(150-(130*(r/5)), 150);
-        context.lineTo(150, 150-r*13);
-        context.lineTo(150, 150);
-        context.closePath();
-        context.strokeStyle = "#2f9aff";
-        context.fillStyle = "#2f9aff";
-        context.fill();
-        context.stroke();
+        drawRectangle(ctx, radius, areaColor);
+        drawSector(ctx, radius, areaColor);
+        drawTriangle(ctx, radius, areaColor);
 
         // axes
-        context.beginPath();
-        context.font = "10px Verdana";
-        context.strokeStyle = "black";
-        context.fillStyle = "black";
-        context.moveTo(150, 0);
-        context.lineTo(150, 300);
-        context.moveTo(150, 0);
-        context.lineTo(145, 15);
-        context.moveTo(150, 0);
-        context.lineTo(155, 15);
-        context.fillText("Y", 160, 10);
-        context.moveTo(0, 150);
-        context.lineTo(300, 150);
-        context.moveTo(300, 150);
-        context.lineTo(285, 145);
-        context.moveTo(300, 150);
-        context.lineTo(285, 155);
-        context.fillText("X", 290, 130);
+        ctx.beginPath();
+        ctx.font = "10px Verdana";
+        ctx.strokeStyle = axesColor;
+        ctx.fillStyle = axesColor;
+        drawArrow(ctx, 150, 300, 150, 0);
+        ctx.fillText("Y", 160, 10);
+        drawArrow(ctx, 0, 150, 300, 150);
+        ctx.fillText("X", 290, 130);
 
-        // Y parts
-        context.moveTo(145, 20);
-        context.lineTo(155, 20);
-        context.fillText(' 5', 160, 20);
-        context.moveTo(145, 46);
-        context.lineTo(155, 46);
-        context.fillText(' 4', 160, 46);
-        context.moveTo(145, 72);
-        context.lineTo(155, 72);
-        context.fillText(' 3', 160, 72);
-        context.moveTo(145, 98);
-        context.lineTo(155, 98);
-        context.fillText(' 2', 160, 98);
-        context.moveTo(145, 124);
-        context.lineTo(155, 124);
-        context.fillText(' 1', 160, 124);
+        drawYAxisLabels(ctx, 160, 124, [' 1', ' 2', ' 3', ' 4', ' 5']);
+        drawYAxisLabels(ctx, 160, 280, ['-4', '-5', '-3', '-2', '-1']);
 
-        context.moveTo(145, 176);
-        context.lineTo(155, 176);
-        context.fillText('-1', 160, 176);
-        context.moveTo(145, 202);
-        context.lineTo(155, 202);
-        context.fillText('-2', 160, 202);
-        context.moveTo(145, 228);
-        context.lineTo(155, 228);
-        context.fillText('-3', 160, 228);
-        context.moveTo(145, 254);
-        context.lineTo(155, 254);
-        context.fillText('-4', 160, 254);
-        context.moveTo(145, 280);
-        context.lineTo(155, 280);
-        context.fillText('-5', 160, 280);
+        drawXAxisLabels(ctx, 20, 160, ['-1', '-2', '-3', '-4', '-5']);
+        drawXAxisLabels(ctx, 176, 160, [' 1', ' 2', ' 3', ' 4', ' 5']);
 
-        // X parts
-        context.moveTo(20, 145);
-        context.lineTo(20, 155);
-        context.fillText('-5', 13, 140);
-        context.moveTo(46, 145);
-        context.lineTo(46, 155);
-        context.fillText('-4', 39, 140);
-        context.moveTo(72, 145);
-        context.lineTo(72, 155);
-        context.fillText('-3', 65, 140);
-        context.moveTo(98, 145);
-        context.lineTo(98, 155);
-        context.fillText('-2', 91, 140);
-        context.moveTo(124, 145);
-        context.lineTo(124, 155);
-        context.fillText('-1', 117, 140);
-
-        context.moveTo(176, 145);
-        context.lineTo(176, 155);
-        context.fillText(' 1', 169, 140);
-        context.moveTo(202, 145);
-        context.lineTo(202, 155);
-        context.fillText(' 2', 195, 140);
-        context.moveTo(228, 145);
-        context.lineTo(228, 155);
-        context.fillText(' 3', 221, 140);
-        context.moveTo(254, 145);
-        context.lineTo(254, 155);
-        context.fillText(' 4', 247, 140);
-        context.moveTo(280, 145);
-        context.lineTo(280, 155);
-        context.fillText(' 5', 273, 140);
-
-        context.closePath();
-        context.strokeStyle = "black";
-        context.fillStyle = "black";
-        context.stroke();
+        ctx.closePath();
+        ctx.stroke();
 
     }
 
